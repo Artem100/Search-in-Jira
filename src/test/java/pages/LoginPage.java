e34480ab7ac5504e3c9f01652f7524ef0a7a948b
apiliuk <lohika.artur@98point6.com>
2018-08-23 14:42:18 +0300

    fix for coockies

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,13 +3,15 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import utils.ConfigProperties;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.title;
 
 public class LoginPage {
 
-    public String jSessionCookies = "";
+    public static String jSessionCookies = "";
 
     public void enterLogin(String login){
         $(By.id("login-form-username")).sendKeys(login);
@@ -26,5 +28,9 @@ public class LoginPage {
     public boolean atRequiredPage() //проверяет, что мы на dashboardPage.
     { Assert.assertEquals(title(), "System Dashboard - Hillel IT School JIRA");
         return true;
+    }
+
+    public void navigateTo(){
+       open(ConfigProperties.getTestProperty("jiraURL") + "/login.jsp");
     }
 }
